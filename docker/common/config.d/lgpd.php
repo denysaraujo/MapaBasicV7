b703045ce943e3a72f3e881ed3517cae662bf2e3
Denílson_S_Araújo <91626655+denysaraujo@users.noreply.github.com>
2023-11-19 00:39:19 -0300

    Update lgpd.php

--- docker/common/config.d/lgpd.php
+++ docker/common/config.d/lgpd.php
@@ -10,13 +10,13 @@ return [
             'buttonText' => i::__('Aceito os termos de uso')
         ],
         'privacyPolicy' => [
-            'title'=>  i::__('Política de Privacidade'),
+            'title'=>  i::__('Política de Privacidade do Mapa Cultural'),
             'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/privacy-policy.html'),
             'buttonText' => i::__('Aceito as políticas de privacidade')
         ],
-        'imageOfUsage' => [
+        'imagesUse' => [
             'title'=>  i::__('Autorização de uso de imagem'),
-            'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/image-of-usage.html'),
+            'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/images-use.html'),
             'buttonText' => i::__('Autorizo o uso de imagem')
         ],
     ]
