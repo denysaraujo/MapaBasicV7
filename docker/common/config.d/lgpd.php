e4e4e63644ec137d4234b7e1918b72f2ff505adf
Denílson_S_Araújo <91626655+denysaraujo@users.noreply.github.com>
2024-02-13 17:57:29 -0300

    Refatoração nas pastas da aplicação

--- docker/common/config.d/lgpd.php
+++ docker/common/config.d/lgpd.php
@@ -6,17 +6,17 @@ return [
     'module.LGPD' => [
         'termsOfUsage'=>[
             'title'=> i::__('Termos de Uso'), 
-            'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/terms-of-usage.html'),
+            'text'=> file_get_contents(__DIR__ . '/lgpd-terms/terms-of-usage.html'),
             'buttonText' => i::__('Aceito os termos de uso')
         ],
         'privacyPolicy' => [
             'title'=>  i::__('Política de Privacidade do Mapa Cultural'),
-            'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/privacy-policy.html'),
+            'text'=> file_get_contents(__DIR__ . '/lgpd-terms/privacy-policy.html'),
             'buttonText' => i::__('Aceito as políticas de privacidade')
         ],
         'imagesUse' => [
             'title'=>  i::__('Autorização de uso de imagem'),
-            'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/images-use.html'),
+            'text'=> file_get_contents(__DIR__ . '/lgpd-terms/images-use.html'),
             'buttonText' => i::__('Autorizo o uso de imagem')
         ],
     ]
