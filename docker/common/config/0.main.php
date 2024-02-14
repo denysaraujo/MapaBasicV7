9bfa44574cebd7d32319de7222c38292c9cb42f8
Denílson_S_Araújo <91626655+denysaraujo@users.noreply.github.com>
2024-02-14 09:09:36 -0300

    Update Files

--- docker/common/config/0.main.php
+++ docker/common/config/0.main.php
@@ -3,7 +3,7 @@
 use MapasCulturais\i;
 
 return [
-    'app.siteName' => 'Mapas Culturais Base Project',
+    'app.siteName' => 'Mapas Culturais Base Project V7',
     'app.siteDescription' => 'O Mapas Culturais é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades',
 
     // Define o tema ativo no site principal. Deve ser informado o namespace do tema e neste deve existir uma classe Theme.
@@ -12,7 +12,7 @@ return [
     // Ids dos selos verificadores. Para utilizar múltiplos selos informe os ids separados por vírgula.
     'app.verifiedSealsIds' => '1', 
 
-    'app.lcode' => 'pt_BR',
+    'app.lcode' => env('APP_LCODE', 'pt_BR,es_ES'),
     //'homeHeader.banner' => 'img/banner-caderno-lpg.jpg',
     //'homeHeader.bannerLink' => 'https://mapacultural.pa.gov.br/files/agent/7/caderno-de-orientacoes-pcac-27-09-2023.pdf',
     //'homeHeader.downloadableLink' => true,
